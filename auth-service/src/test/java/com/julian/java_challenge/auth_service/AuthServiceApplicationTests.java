e74486b8e607d55cebba22087e19db3804acc550
julianlieow <julian.lieow@cognixus.com>
2024-12-26 22:43:52 +0800

    Initial commit

---
+++ auth-service/src/test/java/com/julian/java_challenge/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julian.java_challenge.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
