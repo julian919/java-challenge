d7172e71e97ed7da332b061d38d47c6300d47822
julianlieow <julian.lieow@cognixus.com>
2024-12-28 17:46:58 +0800

    Add eureka-server for service discovery

---
+++ eureka-service/src/test/java/com/julian/java_challenge/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julian.java_challenge.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
